3b82817f8933eb256908d1da9d26a9b3b0b8308a
ArgentumHominem <hombre.hga.plateado@gmail.com>
2023-06-07 18:12:53 -0300

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import './App.css'
+
+
+
+function Midbox(){
+  return(
+    <div className='mid-box'>
+        <div className='little-box'></div>
+        <div className='little-box'></div>
+        <div className='little-box'></div>
+        <div className='little-box'></div>
+        <div className='little-box'></div>
+        <div className='little-box'></div>
+        <div className='little-box'></div>
+        <div className='little-box'></div>
+        <div className='little-box'></div>
+    </div>
+  )
+}
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main>
+      <h1>Sudoku</h1>
+      <section className='sudoku-container'>
+        <Midbox></Midbox>
+        <Midbox></Midbox>
+        <Midbox></Midbox>
+        <Midbox></Midbox>
+        <Midbox></Midbox>
+        <Midbox></Midbox>
+        <Midbox></Midbox>
+        <Midbox></Midbox>
+        <Midbox></Midbox>
+      </section>
+    </main>
+  )
+}
+
+export default App
